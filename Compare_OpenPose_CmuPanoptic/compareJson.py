Corrected error calculation, should work for multiple people as well!

Compare_OpenPose_CmuPanoptic/compareJson.py
@@ -6,64 +6,85 @@ from math import sqrt
 from readCmuPanoptic import readPanopticJson
 from readOpenPose import readOpenPoseJson
 
-def compareJson(panopJson, opJson):
+def compareJson(panopJson, opJson, frame):
 
     #print(panopJson[0].__dict__)
-    #print(opJson[0][0].__dict__)
+    #print(opJson[0].__dict__)
     try:
-        opPerson = opJson[0][0]
+        opPerson = opJson[0]
     except:
-        print("Encountered none opPerson at:"+str(panopJson[0].node))
+        print("OpenPose returned none at:"+str(panopJson[0].node))
         return np.zeros(15)
 
+    # OpenPose to CMU Panoptic Joint Correspondance List
+    # jointCorrespondance[2] = 9 means:
+    # 2nd joint on OpenPose is the 9th joint on CMU Panoptic annotation.
     jointCorrespondance = [1, 0, 9, 10, 11, 3, 4, 5, 2, 12, 13, 14, 6, 7, 8]
-    
+
+    # Create empty array to store joint errors
     err = np.zeros(15)
+
     
-    for frame in range(101):
+    panopPerson = panopJson[frame]
+    panopKp = panopPerson.keypoints
 
-        try:
-            panopPerson = panopJson[frame]
-            opPerson = opJson[0][frame]
-        except:
-            print(frame)
-            print(opJson)
-        
-        if(panopPerson.personNo == opPerson.personNo):
-            panopKp = panopPerson.keypoints
-            opKp = opPerson.keypoints
-            '''
-            print(panopPerson.frame)
-            print(opPerson.frame)
-            print(panopPerson.node)
-            print(opPerson.node)
-            '''
-            #print(opKp[0,0])
-            #print(panopKp[0,1])
+    minErr = float('Inf')
+    
+    for opPerson in opJson:
+        opKp = opPerson.keypoints
 
-            #j = joint number
-            #p = panoptic joint number
-            for j in range(15):
-                if(opKp[0,j] != 0):
-                    p = jointCorrespondance[j]
-                    err[j] = err[j]+sqrt(pow(opKp[0,j]-panopKp[0,p],2)+pow(opKp[1,j]-panopKp[1,p],2))
-
-    result = np.divide(err,101)                            
+       
+        #j = joint number
+        #p = panoptic joint number
+        for j in range(15):
+            if(opKp[0,j] != 0):
+                p = jointCorrespondance[j]
+                err[j] = err[j]+sqrt(pow(opKp[0,j]-panopKp[0,p],2)+pow(opKp[1,j]-panopKp[1,p],2))
+                
+        # Sum all joint errors and check if it's minimum across all persons
+        errSum = sum(err)
+        '''
+        ----DEBUG----
+        print("Person: "+str(opPerson.personNo))
+        print("Frame: "+str(frame))
+        print("Sum of error: " + str(errSum))
+        print("Min Err: " + str(minErr))
+        print(err)
+        ----DEBUG----
+        '''
+        if minErr > errSum:
+            minErr = errSum
+            result = err
+            # if it's the minimum error, save it.
+        err = np.zeros(15)
 
     return result
-    
+
+
+def createDir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 
 if __name__=='__main__':
     filepath = 'E:\Dersler\Master\Computer Vision\Term Project\CompareOpenPoseCmu\\sample'
     sequence = 'sample'
 
+    cwd = os.getcwd()
+    errorFilePath = os.path.join(cwd,"Errors")
+    createDir(errorFilePath)
+
     totalErr = np.zeros(15)
-    
+
     for camNo in range(31):
         panopJson = readPanopticJson(filepath,sequence,0,camNo)
-        opJson = readOpenPoseJson(filepath, camNo)
-        error = compareJson(panopJson, opJson)
-        totalErr = totalErr + error
+        camFilePath = os.path.join(errorFilePath,"%02d" % camNo)
+        createDir(camFilePath)
+        for frame in range(101):
+            finalPath = os.path.join(camFilePath, "%08d" % frame+".npy")
+            opJson = readOpenPoseJson(filepath, camNo, frame)
+            error = compareJson(panopJson, opJson, frame)
+            np.save(finalPath,error)
 
-    print(np.divide(totalErr, 31))
+
     

Compare_OpenPose_CmuPanoptic/readOpenPose.py
@@ -4,7 +4,7 @@ import os
 
 from keyPointClass import keyPointClass
 
-def readOpenPoseJson(kpPath, camNo):
+def readOpenPoseJson(kpPath, camNo, frame):
 
     # Output from OpenPose must be under OpenPose_out folder
     kpPath = kpPath+'\\OpenPose_out'
@@ -22,37 +22,30 @@ def readOpenPoseJson(kpPath, camNo):
     #      |
     #      ...
 
-    opKp = []
-    for camNoFolder in os.listdir(kpPath):
-        camNoPath = os.path.join(kpPath,camNoFolder)
+    # Open the folder with selected camNo
+    node = '%02d' % camNo
+    frameNo = '%08d' % frame
+    panel = '00'
+    camNoPath = os.path.join(kpPath,panel+'_'+node)
+    jsonName = panel+"_"+node+"_"+frameNo+"_keypoints.json"
+    
+    # Create empty keypoint array to kp values
+    keyP = []
 
-        panel = int(camNoFolder[0:2])
-        node = int(camNoFolder[3:5])
+    op_skel_json = os.path.join(camNoPath,jsonName)
+    
+    with open(op_skel_json) as dfile:
+        # Load OpenPose Json file
+        opJson = json.load(dfile)
 
-        if(node != camNo):
-            continue
-        
-        keyP = []
-        
-        for frame, keypointsJson in enumerate(os.listdir(camNoPath)):
-            op_skel_json = os.path.join(camNoPath,keypointsJson)
-            
-            with open(op_skel_json) as dfile:
-                # Load OpenPose Json file
-                opJson = json.load(dfile)
+    for pNo, person in enumerate(opJson['people']):
+        keypoints = np.array(person['pose_keypoints_2d']).reshape((-1,3)).transpose()
+        kp = keyPointClass(pNo, keypoints, panel, node, frame)
+        keyP.append(kp)
 
-            for pNo, person in enumerate(opJson['people']):
-                keypoints = np.array(person['pose_keypoints_2d']).reshape((-1,3)).transpose()
-                kp = keyPointClass(pNo, keypoints, panel, node, frame)
-                
-            keyP.append(kp)
-
-
-        opKp.append(keyP)
-
-    return opKp
+    return keyP
 
 if __name__=='__main__':
     filepath = 'E:\Dersler\Master\Computer Vision\Term Project\CompareOpenPoseCmu\\sample'
     sequence = 'sample'
-    opKp = readOpenPoseJson(filepath)
+    opKp = readOpenPoseJson(filepath,1)